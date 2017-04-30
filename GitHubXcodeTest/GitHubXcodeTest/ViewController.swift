33fc36e40f5179a79cf08c0c9d964425d54867ec
Jan H. Kila <janhkila@iMac-van-Jan.local>
2017-04-30 16:58:23 +0200

    Dit is de initiële project setup.

---
+++ GitHubXcodeTest/GitHubXcodeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubXcodeTest
+//
+//  Created by Jan H. Kila on 30-04-17.
+//  Copyright © 2017 Aleph Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
